7bc284ff4d2506db7c13698d960fffeccf543e8a
Maya Kusumakar <54426748+MayaKusumakar@users.noreply.github.com>
2023-11-29 14:28:25 -0800

    Update inputvalidation.c

--- connectn/inputvalidation.c
+++ connectn/inputvalidation.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+/*
+  function checks if format is correct
+  @numArgsRead: number of args the user entered
+  @numArgsNeed: number of args for the format to be valid
+  @return: true if format is valid
+  */
 bool isValidFormat(const int numArgsRead, const int numArgsNeed)
 {
   bool formatIsGood = numArgsRead == numArgsNeed;
@@ -16,11 +21,14 @@ bool isValidFormat(const int numArgsRead, const int numArgsNeed)
 	}while(character != '\n'); 
   return formatIsGood;
 }
-
+/*
+  function checks if a value is between a lower and upper bound
+  @value: the value to check
+  @lower: lower bound
+  @upper: upper bound
+  @return: true if the function is between the two values
+  */
 bool isBetween(int value, int lower, int upper)
 {
     return value >= lower && value <= upper;
 }
-
-
-
